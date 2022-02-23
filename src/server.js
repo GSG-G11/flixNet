25ea64d11446574059d59f8127917a6b90bb20bb
Abdallah Rabah <abdallah897107@gmail.com>
2022-02-23 17:40:15 +0200

    general fix relates #30

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
 const app = require('./app');
 
 const router = express.Router();
-// const port = 4000;
 app.set('port', process.env.PORT || 4000);
 
 router.get('/', (req, res) => {
@@ -11,10 +9,6 @@ router.get('/', (req, res) => {
 });
 
 app.use('/', router);
-
-// app.listen(port, () => {
-//   console.log(`app listening on port ${port}`);
-// });
 
 app.listen(app.get('port'), () => {
   console.log('App running on port', app.get('port'));
