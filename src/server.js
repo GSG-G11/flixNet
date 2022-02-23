efa15de2757250d6739f3810ae23cfb7fad8cfb6
SalmaIssa96 <salmaissa196@gmail.com>
2022-02-23 13:33:28 +0200

    create static html page for 404 error relates #24

--- src/server.js
+++ src/server.js
@@ -3,19 +3,19 @@ const express = require('express');
 const app = require('./app');
 
 const router = express.Router();
-const port = 4000;
-// app.set('port', process.env.PORT || 4000);
+// const port = 4000;
+app.set('port', process.env.PORT || 4000);
 
 router.get('/', (req, res) => {
   res.send('its working');
-})
+});
 
 app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
-
-// app.listen(app.get('port'), () => {
-//   console.log('App running on port', app.get('port'));
+// app.listen(port, () => {
+//   console.log(`app listening on port ${port}`);
 // });
+
+app.listen(app.get('port'), () => {
+  console.log('App running on port', app.get('port'));
+});
